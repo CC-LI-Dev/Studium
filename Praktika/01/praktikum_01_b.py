8bb16a250dd7a58160227ecb1f5103ed94b24ac2
Conrad Christoph <con.christoph@protonmail.com>
2023-11-13 15:00:09 +0100

    Last Fixes - Check - Cleanup

--- Praktika/01/praktikum_01_b.py
+++ Praktika/01/praktikum_01_b.py
@@ -7,13 +7,14 @@ i = 0
 
 # Input Schleife mit Fehlerfilter
 while x1 <= 0 or y1 <= 0 or x2 <= 0 or y2 <= 0 or x2 < x1 or x2 < x1:
+    if i > 0:
+        print("Mindestens eine der Eingaben ist Fehlerhaft.")
+        print("Anforderungen: 1. Alle Werte >= 0 und 2. x2>=x1 y2>=y1")
     x1 = float(input("Punkt x1:"))
     y1 = float(input("Punkt y1:"))
     x2 = float(input("Punkt x2:"))
     y2 = float(input("Punkt y2:"))
-    if i > 0:
-        print("Mindestens eine der Eingaben ist Fehlerhaft.")
-        print("Anforderungen: 1. Alle Werte >= 0 und 2. x2>=x1 y2>=y1")
+    
     i = i+1
 # Berechnung der Katheten
 line_x1_to_x2 = x2-x1
