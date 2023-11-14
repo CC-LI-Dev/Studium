337c77f5721016525fd85c3c1fcd39cc452f826b
Conrad Christoph <con.christoph@protonmail.com>
2023-11-14 14:12:11 +0100

    Change after Prof suggestion

--- Praktika/01/praktikum_01_a.py
+++ Praktika/01/praktikum_01_a.py
@@ -8,18 +8,18 @@ n = int(input("Näherungsparameter:"))
 approx_value = 1
 x_standin = x*x
 fak = 2
+k = 1
 
 #Berechnung Annäherung
 for i in range(1,n):
     # Vorzeichenwechsel
-    if i % 2 != 0:
-        k = -1
+    k = k*(-1)
     # Berechnung
     approx_value = approx_value + k*((1)/(fak))*x_standin
     # neue Fakultätswerte, höheres Exponential und Reset des Vorzeichens
     fak = fak*(2*i+1)*(2*i+2)  
     x_standin = x_standin*x*x
-    k = 1
+
 
 # Output
 print(approx_value)
